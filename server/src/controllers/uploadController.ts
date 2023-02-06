9af4ec1a5d2bf0c1b42ba35a2c6855f8a9e2f09f
vvv201214 <vvv201214@gmail.com>
2023-02-06 20:21:13 +0530

    folder s3

--- server/src/controllers/uploadController.ts
+++ server/src/controllers/uploadController.ts
@@ -6,21 +6,29 @@ import detectText from '../services/googleOcr';
 import path from 'path';
 import {ocrProccesing} from "../utils/ocrProcessing";
 import {convertPdfToImageBuffer} from '../utils/imageUtil';
+import {getUserDetailAfterRefresh} from "../controllers/authController";
 
 // CatchAsync
 const s3 = new aws.S3();
 
 export const getUploads = (async(req:Request, res:Response, next:NextFunction) => {
   const file = req.file;
-  console.log("file in upload", file)
+  console.log("file in upload", file, (req as any).user)
   if (!file) {
     return res.status(400).send({ error: 'Please provide a file' });
   }
 
-  // configure the parameters for the S3 upload
+  // get paricularUser
+
+  // const particularUser = await getUserDetailAfterRefresh(req, res, next);
+
+  // console.log("particularUser", particularUser)
+
+
+  // configure the parameters for the S3 upload 
   const params = {
     Bucket: process.env.AWS_BUCKET_NAME!,
-    Key: file.originalname,
+    Key: `users/${(req as any).user.firstName + (req as any).user.lastName + (req as any).user.jeevanKhataId }/reports/${(Date.now()) + file.originalname}`,
     Body: file.buffer,
     ContentType: file.mimetype,
     ACL: 'public-read',
@@ -53,4 +61,17 @@ export const getUploads = (async(req:Request, res:Response, next:NextFunction) =
 });
 
 
+
+/*
+data {
+  ETag: '"50144ec9244526f497c161563f449d25"',
+  ServerSideEncryption: 'AES256',
+  VersionId: 'E9rLKolNOdTcOvp1RZtk0NiyP8a5ftMT',
+  Location: 'https://jeevan-khata-test.s3.amazonaws.com/cbc1.jpeg',
+  key: 'cbc1.jpeg',
+  Key: 'cbc1.jpeg',
+  Bucket: 'jeevan-khata-test'
+}
+
+*/
 
